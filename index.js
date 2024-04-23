797e7f3874facf894ed97053daa9e3bce38f8695
SamuelRaranta <163145847+SamuelRaranta@users.noreply.github.com>
2024-04-23 21:55:15 +0700

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.querySelector("nav");
     const menuLinks = document.querySelectorAll("nav ul li a");
+
+    // Mengaktifkan event listener saat menekan link menu
     menuLinks.forEach(link => {
         link.addEventListener("click", function(event) {
             event.preventDefault();
